0137b74ed1f6959dc62bd0040a0d3e3a333e6e51
Lakshmanan1996 <117020820+Lakshmanan1996@users.noreply.github.com>
2026-04-16 13:11:30 +0530

    Update EmployeeController.java

--- src/main/java/com/employee/ems/controller/EmployeeController.java
+++ src/main/java/com/employee/ems/controller/EmployeeController.java
@@ -7,14 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
